903bf81285486ddeae55e93ee8f3a1cfcb40041a
Jayaramgokul <44819189+Jayaramgokul@users.noreply.github.com>
2020-12-23 23:37:02 +0530

    Add files via upload

---
+++ ByteShortInt/Main.java
@@ -0,0 +1,12 @@
+package com.jayaram;
+
+public class Main {
+
+    public static void main(String[] args) {
+	byte Bytevalue=45;
+	short Shortvalue=456;
+	int Intvalue=655;
+	long longvalue=1000L + Bytevalue+Shortvalue+Intvalue;
+        System.out.println(longvalue);
+    }
+}
