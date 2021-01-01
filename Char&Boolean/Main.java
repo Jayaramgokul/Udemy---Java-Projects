5995a7e531cff448e3d93f765abe0029ca28a185
Jayaramgokul <44819189+Jayaramgokul@users.noreply.github.com>
2021-01-01 11:13:52 +0530

    Add files via upload

---
+++ Char&Boolean/Main.java
@@ -0,0 +1,14 @@
+package com.jayaram;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+         double var=20;
+         double var2= 80;
+         double sum=(var+var2)*25;
+         double rem= sum%40;
+         if (rem==20 || rem<20)
+             System.out.println("Total was over the limit");
+    }
+}
