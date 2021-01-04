7f65e33dd62657f92d493cf02dfdb295d036bc7a
Jayaramgokul <44819189+Jayaramgokul@users.noreply.github.com>
2021-01-04 22:01:53 +0530

    Add files via upload

---
+++ Float&Double/Main.java
@@ -0,0 +1,11 @@
+package com.jayaram;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double numofpounds=200d;
+        double numofkilo=numofpounds*0.45359237;
+        System.out.println(numofkilo);
+
+    }
+}
