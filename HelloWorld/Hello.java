ff3c2cd00dd8e67a45368612662ceb4104eff63e
Jayaramgokul <44819189+Jayaramgokul@users.noreply.github.com>
2021-01-04 22:04:28 +0530

    Add files via upload

---
+++ HelloWorld/Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+        int my_firstnumber=5;
+        System.out.println(my_firstnumber);
+
+    }
+}
+
