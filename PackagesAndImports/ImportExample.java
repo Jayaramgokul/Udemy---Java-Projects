1e1dc484c8298c4cd65ae96150274d3197781edf
Jayaramgokul <44819189+Jayaramgokul@users.noreply.github.com>
2021-01-17 12:22:04 +0530

    Add files via upload

---
+++ PackagesAndImports/ImportExample.java
@@ -0,0 +1,15 @@
+package academy.learnprogramming;
+
+import a.b.c.d.MyClass;
+
+import java.util.Random;
+import java.lang.System;
+
+public class ImportExample {
+
+    public static void main(String[] args) {
+        MyClass
+        Random random = new Random();
+        System.out.println(random.nextInt( 5));
+    }
+}
